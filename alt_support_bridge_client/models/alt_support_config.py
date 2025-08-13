ab09aaf7c1b922d039fb672a41b52884d5dc42c2
altAVltd <info@altgroup.media>
2025-08-13 17:01:19 +0530

    testing5

--- alt_support_bridge_client/models/alt_support_config.py
+++ alt_support_bridge_client/models/alt_support_config.py
@@ -5,7 +5,7 @@ class AltSupportConfig(models.Model):
     _name = 'alt.support.config'
     _description = 'Alt Support Configuration'
 
-    server_url = fields.Char(string="Support Server URL 101")
+    server_url = fields.Char(string="Support Server URL 102")
     api_token = fields.Char(string="API Token")
     server_token = fields.Char(string="Server Token", help="Token used by server to authenticate with this client")
 
