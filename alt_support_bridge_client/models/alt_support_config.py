ff91d32e6df45b837bf960fd5287ed6e1e58c699
Prefortune Technologies LLP <info@prefortune.com>
2025-08-14 12:48:36 +0530

    Update alt_support_config.py

--- alt_support_bridge_client/models/alt_support_config.py
+++ alt_support_bridge_client/models/alt_support_config.py
@@ -6,7 +6,7 @@ class AltSupportConfig(models.Model):
     _description = 'Alt Support Configuration'
 
     server_url = fields.Char(string="Support Server URL Local 101.11")
-    server_url2 = fields.Char(string="Support Server URL 202")
+    server_url2 = fields.Char(string="Support Server URL 2022")
     server_url3 = fields.Char(string="Support Server URL 203")
 
 
